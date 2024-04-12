bef2b3479dfc6eb77f7895581aed29e1301faf2d
Grégory <gregory95am@gmail.com>
2024-04-12 14:22:31 +0200

    0.1
    Initial Release

---
+++ js/pages/List.js
@@ -0,0 +1,187 @@
+import { store } from "../main.js";
+import { embed } from "../util.js";
+import { score } from "../score.js";
+import { fetchEditors, fetchList } from "../content.js";
+
+import Spinner from "../components/Spinner.js";
+import LevelAuthors from "../components/List/LevelAuthors.js";
+
+const roleIconMap = {
+	owner: "crown",
+	admin: "user-gear",
+	helper: "user-shield",
+	dev: "code",
+	trial: "user-lock",
+};
+
+export default {
+	components: { Spinner, LevelAuthors },
+	template: `
+        <main v-if="loading">
+            <Spinner></Spinner>
+        </main>
+        <main v-else class="page-list">
+            <div class="list-container">
+                <table class="list" v-if="list">
+                    <tr v-for="([level, err], i) in list">
+                        <td class="rank">
+                            <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-else class="type-label-lg">Legacy</p>
+                        </td>
+                        <td class="level" :class="{ 'active': selected == i, 'error': !level }">
+                            <button @click="selected = i">
+                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="level-container">
+                <div class="level" v-if="level">
+                    <h1>{{ level.name }}</h1>
+                    <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
+                    <div v-if="level.showcase" class="tabs">
+                        <button class="tab type-label-lg" :class="{selected: !toggledShowcase}" @click="toggledShowcase = false">
+                            <span class="type-label-lg">Video</span>
+                        </button>
+
+                    </div>
+                    <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
+                    <ul class="stats">
+                        <li>
+                            <div class="type-title-sm">Points when completed</div>
+                            <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
+                        </li>
+                        <li>
+                            <div class="type-title-sm">ID</div>
+                            <p>{{ level.id }}</p>
+                        </li>
+                    </ul>
+                    <h2>Records</h2>
+                    <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
+                    <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
+                    <p v-else>This level does not accept new records.</p>
+                    <table class="records">
+                        <tr v-for="record in level.records" class="record">
+                            <td class="percent">
+                                <p>{{ record.percent }}%</p>
+                            </td>
+                            <td class="user">
+                                <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
+                            </td>
+                            <td class="mobile">
+                                <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
+                            </td>
+                            <td class="hz">
+                                <p>{{ record.hz }}Hz</p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
+                    <p>(ノಠ益ಠ)ノ彡┻━┻</p>
+                </div>
+            </div>
+            <div class="meta-container">
+                <div class="meta">
+                    <div class="errors" v-show="errors.length > 0">
+                        <p class="error" v-for="error of errors">{{ error }}</p>
+                    </div>
+                    <div class="og">
+                        <p class="type-label-md">Original List by <a href="https://me.redlimerl.com/" target="_blank">RedLime</a></p>
+                    </div>
+                    <template v-if="editors">
+                        <h3>List Editors</h3>
+                        <ol class="editors">
+                            <li v-for="editor in editors">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
+                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
+                                <p v-else>{{ editor.name }}</p>
+                            </li>
+                        </ol>
+                    </template>
+                    <h3>Submission Requirements</h3>
+                    <p>
+                        Achieved the record without using banned hacks. <a href="https://docs.google.com/spreadsheets/d/1CgqiIqhnvTv6Wg_GKA7OnNLaW-Hgt_OtHgHCZWTqBMs">See the guidelines here.</a>
+                    </p>
+                    <p>
+                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                    </p>
+                    <p>
+                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                    </p>
+                    <p>
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                    </p>
+                    <p>
+                        The recording must also show the player hit the endwall, and display end stats, or the completion will be invalidated.
+                    </p>
+                    <p>
+                        Do not use secret routes or bug routes
+                    </p>
+                    <p>
+                        Do not use easy modes, only a record of the unmodified level qualifies
+                    </p>
+                    <p>
+                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                    </p>
+                </div>
+            </div>
+        </main>
+    `,
+	data: () => ({
+		list: [],
+		editors: [],
+		loading: true,
+		selected: 0,
+		errors: [],
+		roleIconMap,
+		store,
+		toggledShowcase: false,
+	}),
+	computed: {
+		level() {
+			return this.list[this.selected][0];
+		},
+		video() {
+			if (!this.level.showcase) {
+				return embed(this.level.verification);
+			}
+
+			return embed(
+				this.toggledShowcase
+					? this.level.showcase
+					: this.level.verification
+			);
+		},
+	},
+	async mounted() {
+		// Hide loading spinner
+		this.list = await fetchList();
+		this.editors = await fetchEditors();
+
+		// Error handling
+		if (!this.list) {
+			this.errors = [
+				"Failed to load list. Retry in a few minutes or notify list staff.",
+			];
+		} else {
+			this.errors.push(
+				...this.list
+					.filter(([_, err]) => err)
+					.map(([_, err]) => {
+						return `Failed to load level. (${err}.json)`;
+					})
+			);
+			if (!this.editors) {
+				this.errors.push("Failed to load list editors.");
+			}
+		}
+
+		this.loading = false;
+	},
+	methods: {
+		embed,
+		score,
+	},
+};
---
+++ js/util.js
@@ -0,0 +1,38 @@
+// https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
+export function getYoutubeIdFromUrl(url) {
+	return url.match(
+		/.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/,
+	)?.[1] ?? '';
+}
+
+export function embed(video) {
+	return `https://www.youtube.com/embed/${getYoutubeIdFromUrl(video)}`;
+}
+
+export function localize(num) {
+	return num.toLocaleString(undefined, { minimumFractionDigits: 2 });
+}
+
+export function getThumbnailFromId(id) {
+	return `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
+}
+
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+export function shuffle(array) {
+	let currentIndex = array.length, randomIndex;
+
+	// While there remain elements to shuffle.
+	while (currentIndex != 0) {
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
+	}
+
+	return array;
+}
